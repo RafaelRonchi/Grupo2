7f81c899013a5f4b6503355822eda407a8070f31
MiraiDinelli <felnd14@outlook.com>
2023-07-03 15:37:54 -0300

    primeiro commit

---
+++ BibliotecaDevs2Blu/Leitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDevs2Blu
+{
+    public class Leitor
+    {
+        public string Nome { get; set; }
+        public List<Emprestimo> Emprestimos { get; set; }
+        public Leitor(string nome)
+        {
+            Nome = nome;
+            Emprestimos = new List<Emprestimo>();
+        }
+        public void EmprestarLivro(Emprestimo livro)
+        {
+            Emprestimos.Add(livro);
+        }
+        public void DevolverLivro(Emprestimo livro)
+        {
+            Emprestimos.Remove(livro);
+        }
+    }
+}
